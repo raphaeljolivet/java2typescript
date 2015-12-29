780fa3b130874bb5d162b183ba6704f0ef3e0330
Ats Uiboupin <ats.uiboupin@iglu.ee>
2015-12-29 21:32:09 +0200

    fixed command line tests issue - can't rely on specific order of classes/fileds/methods, so file content equality can't be used. Now just checking that all lines are present

--- java2typescript-jackson/src/test/java/java2typescript/jackson/module/util/ExpectedOutputChecker.java
+++ java2typescript-jackson/src/test/java/java2typescript/jackson/module/util/ExpectedOutputChecker.java
@@ -3,20 +3,35 @@ package java2typescript.jackson.module.util;
 import java.io.IOException;
 import java.io.Writer;
 import java.net.URL;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.Assert;
 
 import com.google.common.base.Charsets;
+import com.google.common.collect.Lists;
 import com.google.common.io.Resources;
 
 public class ExpectedOutputChecker {
 
 	public static void checkOutputFromFile(Writer out) {
-		Assert.assertEquals(getExpectedOutput(), out.toString());
+		compareFileContent(out);
+	}
+
+	private static void compareFileContent(Writer out) {
+		// Can't rely on specific order of classes/fields/methods, so file content equality can't be used.
+		// Using naive approach to check that actual output contains exactly the same lines as expected output
+		Assert.assertEquals(getLinesAlphabetically(getExpectedOutput()), getLinesAlphabetically(out.toString()));
+	}
+
+	private static List<String> getLinesAlphabetically(String s) {
+		List<String> lines = Lists.newArrayList(s.split("\\n"));
+		Collections.sort(lines);
+		return lines;
 	}
 
 	private static String getExpectedOutput() {
-		StackTraceElement testMethodStackTraceElem = new Throwable().getStackTrace()[2];
+		StackTraceElement testMethodStackTraceElem = new Throwable().getStackTrace()[3];
 		String testMethodName = testMethodStackTraceElem.getMethodName();
 		String className = testMethodStackTraceElem.getClassName();
 		return getFileContent(className.replace('.', '/') + "." + testMethodName + ".d.ts");
